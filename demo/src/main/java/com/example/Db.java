c5e7175e64f57477344eae6f5ce2a4c3a7615664
Gheruha <maxgheruha@gmail.com>
2024-06-09 15:17:46 +0300

    Insert and Read functions

--- demo/src/main/java/com/example/Db.java
+++ demo/src/main/java/com/example/Db.java
@@ -2,7 +2,10 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Logger;
 
 public class Db {
@@ -14,8 +17,57 @@ public class Db {
         try {
             if (connection == null || connection.isClosed()) {
                 connection = DriverManager.getConnection("jdbc:sqlite:practica.db");
-                logger.info("Connected to Database");
+                logger.info("\n\nConnected to Database ✅");
+                createTable();
+                readDataFiliale();
             }
+        } catch (SQLException e) {
+            logger.info(e.toString());
+        }
+    }
+
+    private void createTable() {
+        String query = "CREATE TABLE IF NOT EXISTS filiale ( id_filiala INTEGER PRIMARY KEY AUTOINCREMENT, nume TEXT DEFAULT NULL, adresa TEXT DEFAULT NULL,  telefon TEXT DEFAULT NULL );";
+
+        try (PreparedStatement statement = connection.prepareStatement(query);) {
+            statement.executeUpdate();
+            logger.info("Table created ✅");
+            // insertFiliale();
+        } catch (SQLException e) {
+            logger.info(e.toString());
+        }
+    }
+
+    private void insertFiliale() {
+        String query = "insert into filiale(nume , adresa , telefon) values('MoldaSig' , 'Soseaua pandurilor' , '022-99-23-43');";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.executeUpdate();
+            logger.info("Inserted data in Filiale ✅");
+        } catch (SQLException e) {
+            logger.info(e.toString());
+        }
+    }
+
+    private void readDataFiliale() {
+        String query = "select * from filiale";
+        try (PreparedStatement statement = connection.prepareStatement(query);
+                ResultSet result = statement.executeQuery()) {
+
+            while (result.next()) {
+                int idFiliala = result.getInt("id_filiala");
+                String nume = result.getString("nume");
+                String adresa = result.getString("adresa");
+                String telefon = result.getString("telefon");
+
+                System.out.println("Filiala Details:");
+                System.out.println("ID: " + idFiliala);
+                System.out.println("Nume: " + nume);
+                System.out.println("Adresa: " + adresa);
+                System.out.println("Telefon: " + telefon);
+                System.out.println("----------");
+            }
+
         } catch (SQLException e) {
             logger.info(e.toString());
         }
